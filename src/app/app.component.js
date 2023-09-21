4c6785f8ca44f5b6688b3c7e86722297cea2f5d5
AidenWik <142683824+AidenWik@users.noreply.github.com>
2023-09-21 13:11:19 -0400

    fixxxx

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,50 +1,67 @@
-//toggle button
-const details = document.querySelector('summary');
+// JavaScript to toggle visibility of the paragraph descriptions
+const toggleDetailsButtons = document.querySelectorAll('.toggle-details');
+const paragraphsSections = document.querySelectorAll('.paragraphs');
 
-document.querySelector('button[data-toggle-btn]').addEventListener('click', (e) => {
-  if (details.parentNode.getAttribute('open')) {
-        details.parentNode.removeAttribute('open');
-  }
-  else {
-    details.parentNode.setAttribute('open','open');    
-  }
+toggleDetailsButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    paragraphsSections[index].classList.toggle('hidden');
+  });
 });
 
-//duplicate button
-document.querySelector('.duplicate').addEventListener('click', function(e) {
-  const cardToClone = document.querySelector('.card').cloneNode(true);
-  document.body.appendChild(cardToClone);
+// JavaScript for cloning cards
+document.querySelector('#duper').addEventListener('click', (e) => {
+  const cardContainer = document.querySelector('.card-container');
+  const cardWrapperClone = document.querySelector('.card-wrapper').cloneNode(true);
+
+  // Add unique classes to the cloned elements
+  cardWrapperClone.classList.add('cloned-card');
+  cardWrapperClone.querySelector('.toggle-details').textContent = 'Details';
+
+  cardContainer.appendChild(cardWrapperClone);
+
+  // Set up event listeners for the cloned button
+  const clonedToggleDetailsButton = cardWrapperClone.querySelector('.toggle-details');
+  const clonedParagraphsSection = cardWrapperClone.querySelector('.paragraphs');
+
+  clonedToggleDetailsButton.addEventListener('click', () => {
+    clonedParagraphsSection.classList.toggle('hidden');
+  });
+});
+document.querySelector('#deleteLastCard').addEventListener('click', (e) => {
+// Code for deleting the last card
+const cardContainer = document.querySelector('.card-container');
+const cardWrappers = document.querySelectorAll('.card-wrapper');
+
+// Check if there's at least one card to delete
+if (cardWrappers.length > 1) {
+    const lastCard = cardWrappers[cardWrappers.length - 1];
+    cardContainer.removeChild(lastCard);
+}
 });
 
-//delete button
-document.querySelector('#deletecard').addEventListener('click', function(e) {
-  let wantsTo = confirm("Are you sure?");
-  if (wantsTo) {
-    if (document.querySelector('.card:last-child') !== document.querySelector('.card')) {
-      document.querySelector('.card:last-child').remove();      
-    }
-    else {
-      alert("Cannot original last card");
-    }
-  }
+document.querySelector('.card-wrapper').addEventListener('mouseover', (e) => {
+const cardWrapper = document.querySelector('.card-wrapper');
+cardWrapper.classList.toggle('alt-bg2');
 });
 
-//change title button
-document.querySelector('.changetxt').addEventListener('click', function(e) {
-  let name = "Something else";
-  if (name) {
-    document.querySelector('.card h3').innerText = name;
-  }
+document.querySelector('.card-wrapper').addEventListener('mouseleave', (e) => {
+const cardWrapper = document.querySelector('.card-wrapper');
+cardWrapper.classList.toggle('alt-bg2');
 });
 
-//change background
-const card = document.querySelector('#card');
-        let backgroundColor = false;
-    document.querySelector('#changebackground').addEventListener('click', function(e) {
-            if (backgroundColor) {
-                card.style.backgroundColor = 'white';
-            } else {
-                card.style.backgroundColor = 'gray';
-            }
-            backgroundColor = !backgroundColor;
-        });
+document.querySelector('#title').addEventListener('click', (e) => {
+const headings = document.querySelectorAll('.sheetzLbl'); // Corrected class name
+headings.forEach((heading) => {
+heading.textContent = "something else";
+});
+});
+
+
+
+document.querySelector('#background').addEventListener('click', (e) => {
+const cardWrappers = document.querySelectorAll('.card-wrapper');
+/* loop to change each existing iteration*/
+cardWrappers.forEach((cardWrapper) => {
+  cardWrapper.classList.toggle('alt-bg');
+});
+});
